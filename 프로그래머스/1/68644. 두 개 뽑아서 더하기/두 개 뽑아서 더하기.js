5db027f2456e52f78bc3aa82f7b7af62c92a35a2
kanglocal <96380950+kanglocal@users.noreply.github.com>
2023-10-18 10:25:09 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.31 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,4 +1,16 @@
 function solution(numbers) {
-    var answer = [];
+    let answer = [];
+    let answerSet = new Set();
+    
+    for(let i in numbers){
+        for(let j in numbers){
+            if(i !== j){
+                answerSet.add(numbers[i] + numbers[j]);        
+            }
+        }
+    }
+    
+    answer = [...answerSet].sort((a, b) => { return a-b; });
+    
     return answer;
 }
