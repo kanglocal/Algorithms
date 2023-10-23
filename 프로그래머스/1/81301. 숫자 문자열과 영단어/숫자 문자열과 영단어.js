3c357a30845897b4205fb82e8bb5d1a556f0bc70
kanglocal <96380950+kanglocal@users.noreply.github.com>
2023-10-23 09:34:14 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,25 +1,12 @@
 function solution(s) {
-    var answer = 0;
-    
-    let wordArr = ['zero','one','two','three','four','five','six','seven','eight','nine'];
-    let originNum ={
-        zero: 0,
-        one: 1,
-        two: 2,
-        three: 3,
-        four: 4,
-        five: 5,
-        six: 6,
-        seven: 7,
-        eight: 8,
-        nine: 9,
+
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
+
+    for(let i=0; i< numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i);
     }
-    
-    for(let word of wordArr){
-        s = s.replaceAll(word,originNum[word]);
-    }
-    
-    answer = Number(s);
-    
-    return answer;
+
+    return Number(answer);
 }
