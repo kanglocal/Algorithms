40d3ebb282a448283d39864de3e88bbfb1b8ef22
kanglocal <96380950+kanglocal@users.noreply.github.com>
2023-10-23 09:48:32 +0900

    [level 1] Title: 최소직사각형, Time: 7.49 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,21 +1,4 @@
 function solution(sizes) {
     var answer = 0;
-    
-    let sortedSizes = [...sizes];
-    let widthArr = [];
-    let heightArr = [];
-    
-    // 정렬
-    for(let size of sortedSizes){
-        if(size[0] < size[1]){
-            size.sort((a,b) => {
-                return b-a;
-            })
-        }
-        widthArr.push(size[0]);
-        heightArr.push(size[1]);
-    }
-    
-    answer = Math.max(...widthArr) * Math.max(...heightArr)
     return answer;
 }
