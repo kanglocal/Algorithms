0f5ddf0172755f10f921ae53492768c123b5970f
kanglocal <96380950+kanglocal@users.noreply.github.com>
2023-10-24 13:14:42 +0900

    [level unrated] Title: 캐릭터의 좌표, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,23 +1,4 @@
 function solution(keyinput, board) {
     var answer = [];
-    
-    const num = {
-        left:[-1,0],
-        right:[1,0],
-        up:[0,1],
-        down:[0,-1]
-    }
-    const maxW = Math.floor(board[0] / 2 );
-    const maxH = Math.floor(board[1] / 2);
-    let w = 0;
-    let h = 0;
-    
-    keyinput.forEach((key)=>{
-        const [addW,addH] = num[key];
-        if(Math.abs(w + addW) <= maxW){ w += addW;} 
-        if(Math.abs(h + addH) <= maxH){ h += addH;}
-    })
-    
-    answer = [w, h];
     return answer;
 }
