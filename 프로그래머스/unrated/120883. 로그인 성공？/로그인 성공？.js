fa82f9aed1e364863083a06fd61851d39e8ab710
kanglocal <96380950+kanglocal@users.noreply.github.com>
2023-10-30 10:32:13 +0900

    [level unrated] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.js
@@ -1,24 +1,5 @@
 function solution(id_pw, db) {
-    var answer = '';
-    const [id, pw] = id_pw;
-    console.log(id + ", " + pw);
-    
-    for(let data of db){
-        if(data[0] === id && data[1] === pw){
-            answer = 'login';
-            break;
-        }
-        if(data[0] === id){
-            answer = 'wrong pw';
-            
-        }
-        if(data[0] !== id && data[1] !== pw){
-            answer = 'fail';    
-        }
-        
-    }
-    
-    
-    
-    return answer;
+  const [id, pw] = id_pw;
+  const map = new Map(db);
+  return map.has(id) ? (map.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
 }
