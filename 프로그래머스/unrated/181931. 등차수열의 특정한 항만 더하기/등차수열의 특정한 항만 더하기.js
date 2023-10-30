9f054ca8adf1c842734bf4a573f8b0eb0e12995a
kanglocal <96380950+kanglocal@users.noreply.github.com>
2023-10-30 10:37:17 +0900

    [level unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,13 @@
+function solution(a, d, included) {
+    var answer = 0;
+    
+    let value = a;
+    for(let i = 0; i < included.length; i++){
+        if(included[i]){
+            answer += value;
+        }
+        value += d;
+    }
+    
+    return answer;
+}
