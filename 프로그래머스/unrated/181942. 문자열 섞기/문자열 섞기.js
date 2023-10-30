b0c5aedb879df06738c2b1e2ef76b72a8c798731
kanglocal <96380950+kanglocal@users.noreply.github.com>
2023-10-30 10:44:26 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -2,8 +2,8 @@ function solution(str1, str2) {
     var answer = '';
     
     for(let i= 0; i < str1.length; i++){
-        answer += str1[i];
-        answer += str2[i];
+        answer += str1[i] + str2[i];
+        
     }
     
     return answer;
