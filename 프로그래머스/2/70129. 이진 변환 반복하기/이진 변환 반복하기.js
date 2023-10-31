c27130637d18253129282c25e8b8360293eebaad
kanglocal <96380950+kanglocal@users.noreply.github.com>
2023-10-31 10:37:15 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 9.52 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,24 +1,4 @@
 function solution(s) {
     var answer = [];
-    let copy = s;
-    let zeroCnt = 0;
-    let processCnt = 0;
-    
-    while(true){
-        if(Number(copy) === 1){
-            break;
-        }
-        
-        for(let letter of copy){
-            if(letter === '0') zeroCnt++;
-        }
-        
-        copy = copy.replaceAll('0','');
-        copy = (copy.length).toString(2);
-        processCnt++;
-    }
-    
-    answer = [processCnt, zeroCnt];
-    
     return answer;
 }
