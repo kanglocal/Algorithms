e0871a401d8efa7b1899bb448059b827fd7a7ea8
강현지 <96380950+kanglocal@users.noreply.github.com>
2023-11-20 10:42:47 +0900

    [level 1] Title: 추억 점수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,13 +1,13 @@
 function solution(name, yearning, photo) {
     var answer = [];
     
-    for(let items of photo) {
+    photo.forEach((items) => {
         let sum = items.reduce((acc, cur,index) => {
             acc += yearning[name.indexOf(cur)] ?? 0;
             return acc;
         }, 0);
         answer.push(sum);
-    }
+    })
     
     return answer;
 }
