10ffd78ff4a24bb3039ddcf66dfe067467f0868d
강현지 <96380950+kanglocal@users.noreply.github.com>
2023-11-24 10:46:55 +0900

    [level 2] Title: 구명보트, Time: 10.36 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,27 +1,10 @@
 function solution(people, limit) {
-//     var answer = 0;
-    
-//     people.sort((a,b) => a-b);
-    
-//     let minusCnt = 0;
-//     for(let i = 0; i < people.length; i++){
-//         for(let j = people.length; j > 0; j--){
-//             if(i != j){
-//                 if(people[i] + people[j] <= limit){
-//                     minusCnt ++;
-//                     continue;
-//                 }    
-//             }
-            
-//         }
-//     }
-//     answer = people.length - minusCnt;
-    
-//     return answer;
     
     people.sort(function(a, b){return a-b});
+    
     for(var i=0, j=people.length-1; i < j; j--) {
         if( people[i] + people[j] <= limit ) i++;
     }    
+    
     return people.length-i;
 }
