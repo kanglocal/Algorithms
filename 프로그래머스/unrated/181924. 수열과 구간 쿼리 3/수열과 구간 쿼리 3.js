98f0ebca7a56e0cdea992c9d461c5f46fd7ed25d
강현지 <96380950+kanglocal@users.noreply.github.com>
2023-11-28 16:13:32 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.42 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,10 +1,7 @@
 function solution(arr, queries) {
     
-    for(let query of queries){
-        let s = arr[query[0]];
-        let e = arr[query[1]];
-        arr.splice(query[0], 1, e);
-        arr.splice(query[1], 1, s);
+    for(let [i, j] of queries){
+        [arr[i], arr[j]] = [arr[j], arr[i]];
     }
     
     return arr;
