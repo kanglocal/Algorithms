79507020e1922798f333ed2023d622dfb0bc565a
강현지 <96380950+kanglocal@users.noreply.github.com>
2023-12-01 04:45:55 +0900

    [level unrated] Title: 배열 만들기 5, Time: 1.02 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,9 +1,3 @@
 function solution(intStrs, k, s, l) {
-    var answer = [];
-    
-    for(let str of intStrs){
-        let num = Number(str.substr(s,l));
-        if(num > k) answer.push(num);
-    }
-    return answer;
+    return intStrs.map((v) => +v.substr(s, l)).filter((v) => v > k);
 }
