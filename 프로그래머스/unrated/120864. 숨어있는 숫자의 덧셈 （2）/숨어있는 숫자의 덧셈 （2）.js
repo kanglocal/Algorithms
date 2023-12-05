51849d008760add3ebf6518ac4e98d4e13d14061
강현지 <96380950+kanglocal@users.noreply.github.com>
2023-12-06 03:21:57 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,11 +1,4 @@
 function solution(my_string) {
-    var answer = 0;
-    let arr = my_string.replaceAll(/[a-zA-Z]/g,'-').split('-')
     
-    answer = arr.reduce((acc,cur,i) => {
-        return acc + Number(cur);
-    }, 0)
-    
-    
-    return answer;
+    return my_string.split(/\D+/).reduce((acc, cur) => acc + Number(cur), 0);
 }
