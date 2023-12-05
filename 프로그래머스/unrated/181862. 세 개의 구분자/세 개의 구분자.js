c967fc5e34638db6dd51a25f1cca67af79c77c4e
강현지 <96380950+kanglocal@users.noreply.github.com>
2023-12-06 03:50:07 +0900

    [level unrated] Title: 세 개의 구분자, Time: 15.13 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,12 +1,7 @@
 function solution(myStr) {
     var answer = [];
     
-    let arr = myStr.replaceAll(/[abc]/g,'-').split('-');
-    for(let v of arr){
-        if(v) answer.push(v);
-    }
+    answer = myStr.split(/[abc]/).filter(str => str);
     
-    if(answer.length === 0) answer.push('EMPTY');
-    
-    return answer;
+    return answer.length ? answer : ['EMPTY'] ;
 }
