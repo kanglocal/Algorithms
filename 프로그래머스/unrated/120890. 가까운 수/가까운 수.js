c16de3a30fa5058dea7c91706e575756851eacec
강현지 <96380950+kanglocal@users.noreply.github.com>
2023-12-06 03:31:50 +0900

    [level unrated] Title: 가까운 수, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/unrated/120890. 가까운 수/가까운 수.js
@@ -1,10 +1,7 @@
 function solution(array, n) {
     var answer = 0;
-    array.sort((a,b) => a-b);
-    console.log(array);
     
-    let newArr = array.map((v) => Math.abs(n-v));
-    
+    let newArr = array.sort((a,b) => a-b).map((v) => Math.abs(n-v));
     answer = array[newArr.indexOf(Math.min(...newArr))];
     
     return answer;
