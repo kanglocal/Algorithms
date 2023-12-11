b790d7c58a889404dbc1d008316ce12a1d6e4d0a
강현지 <96380950+kanglocal@users.noreply.github.com>
2023-12-11 18:36:35 +0900

    [level unrated] Title: 컨트롤 제트, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,12 +1,8 @@
 function solution(s) {
     var answer = 0;
     
-    answer = s.split(' ').filter((v, i, arr) => {
-        if(v !== 'Z'){
-            return arr[i+1] !== 'Z';
-        }
-        return v !== 'Z';
-    }).reduce((acc,cur) => Number(acc) + Number(cur), 0);
+    answer = s.split(' ').filter((v, i, arr) => v !== 'Z' && arr[i+1] !== 'Z')
+        .reduce((acc,cur) => +acc + +cur, 0);
     
     return answer;
 }
