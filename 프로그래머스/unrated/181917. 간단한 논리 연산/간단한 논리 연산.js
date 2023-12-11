18393c1675bc73f5678b3c226ff0237dd502b9e9
강현지 <96380950+kanglocal@users.noreply.github.com>
2023-12-11 17:17:16 +0900

    [level unrated] Title: 간단한 논리 연산, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -0,0 +1,11 @@
+function solution(x1, x2, x3, x4) {
+    var answer = true;
+    
+    answer = (x1 || x2) && (x3 || x4);
+    
+    return answer;
+}
+
+// ∨ = ||
+// ∧ = &&
+// (x1 || x2) && (x3 || x4)
