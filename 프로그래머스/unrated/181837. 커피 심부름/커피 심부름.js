cddcb2efff947a55cd097c1292346c8f8e98a944
강현지 <96380950+kanglocal@users.noreply.github.com>
2023-12-12 13:21:42 +0900

    [level unrated] Title: 커피 심부름, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -1,26 +1,9 @@
 function solution(order) {
     var answer = 0;
     
-    let cost = {
-        americano : 4500,
-        anything : 4500,
-        cafelatte : 5000,
-    }
-    
-    let menus = ['americano', 'cafelatte', 'anything'];
-    let orders = [];
-    
-    for(let orderItem of order){
-        for(let menu of menus){
-            if(orderItem.includes(menu)){
-                orders.push(menu);
-            }
-        }
-    }
-    
-    orders.forEach((menu)=> {
-        answer += cost[menu];
-    })
+    answer = order.reduce((acc, cur) => {
+        return acc + (cur.includes('cafelatte') ? 5000 : 4500)
+    }, 0);
     
     return answer;
 }
