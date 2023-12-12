d74a15e4a0eb0416b8297846da343c9e22fee500
강현지 <96380950+kanglocal@users.noreply.github.com>
2023-12-12 13:23:39 +0900

    [level unrated] Title: qr code, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181903. qr code/qr code.js
@@ -0,0 +1,11 @@
+function solution(q, r, code) {
+    var answer = '';
+    
+    for(let i in code){
+        if(i%q === r){
+            answer += code[i];
+        }    
+    }
+    
+    return answer;
+}
