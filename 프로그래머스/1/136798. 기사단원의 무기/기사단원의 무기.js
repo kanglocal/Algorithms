5e6b47b818d67a3e868d49bec417c66cdc60c673
강현지 <96380950+kanglocal@users.noreply.github.com>
2025-01-07 23:16:48 +0900

    [level 1] Title: 기사단원의 무기, Time: 136.92 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,19 +1,19 @@
 function solution(number, limit, power) {
   let answer = 0;
-  
-  // number 를 돌면서 각각 num의 약수 개수 뽑기
+
+  // number 를 돌면서 각각 num의 약수 개수를 뽑아내.
   // 뽑은 약수 개수가 limit을 초과하면 power로 대체.
   // 위에서 만든 애를 answer 에 더한다.
   
   for(let num = 1; num <= number; num++) {
-    let weaponPower = getMeasureCnt(num);
+    let weaponPower = getDivisorCnt(num);
     if(weaponPower > limit) weaponPower = power;
     answer += weaponPower;
   }
   return answer;
 }
 
-const getMeasureCnt = (num) => {
+const getDivisorCnt = (num) => {
   let count = 0;
   for(let i = 1; i <= Math.sqrt(num); i++) {
     if(num % i === 0) {
