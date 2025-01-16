b6a760b328b2f20089b7f6c5ffb091e1f9959621
강현지 <96380950+kanglocal@users.noreply.github.com>
2025-01-16 23:11:55 +0900

    [level 1] Title: 소수 만들기, Time: 27.25 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,28 @@
+function solution(nums) {
+  var answer = 0;
+
+  const numLength = nums.length;
+
+  for(let index1 = 0; index1 < numLength - 2; index1++){
+    for(let index2 = index1 + 1; index2 < numLength - 1; index2++){
+      for(let index3 = index2 + 1; index3 < numLength; index3++) {
+        const num = nums[index1] + nums[index2] + nums[index3]
+        if(isPrime(num)) answer++
+      }
+    }
+  }
+
+  return answer;
+}
+
+const isPrime = (number) => {
+  if(number === 1) return false;
+
+  let cnt = 1;
+
+  for(let i = 2; i <= Math.sqrt(number); i++) {
+    if(number % i === 0) return false;
+  }
+
+  if(cnt === 1) return true
+}
